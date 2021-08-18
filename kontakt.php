6b6280c626b18a1461b0929088ddc920b3856c42
zile028 <zile028@gmail.com>
2021-08-18 02:01:58 +0200

    finish

--- kontakt.php
+++ kontakt.php
@@ -1,5 +1,3 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 <?php include "partials/top.php" ?>
 <?php include 'partials/social.php' ?>
 
